a074887407339c6b3d9f0e97d2a693842af0dda6
alfonsopintos <alfonsopintos@gmail.com>
2014-10-01 21:59:15 -0400

    interval migration and dates method with gem installed

---
+++ db/migrate/20141002015743_add_interval_to_event_recurrences.rb
@@ -0,0 +1,5 @@
+class AddIntervalToEventRecurrences < ActiveRecord::Migration
+  def change
+    add_column :event_recurrences, :interval, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002003847) do
+ActiveRecord::Schema.define(version: 20141002015743) do
 
   create_table "event_recurrences", force: true do |t|
     t.string   "bill_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141002003847) do
     t.string   "every"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "interval"
   end
 
 end
