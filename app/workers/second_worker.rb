df7e33898ab49a38a7d062eafd712393e052e42f
alfonsopintos <alfonsopintos@gmail.com>
2014-10-05 21:25:07 -0400

    second worker running off rake

--- app/workers/second_worker.rb
+++ app/workers/second_worker.rb
@@ -1,55 +1,69 @@
 class SecondWorker
-  include Sidekiq::Worker
 
+  include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform
-  tomorrow = Date.tomorrow
-  EventRecurrence.all.each do |event|
-    event.dates.each do |duedate|
-      if (duedate == tomorrow) && (event.contact_method == 'both')
-        text
-        call
-      elsif (duedate == tomorrow) && (event.contact_method == 'text')
-        text
-      elsif (duedate == tomorrow) && (event.contact_method == 'phone call')
-        call
+    tomorrow = Date.tomorrow
+    EventRecurrence.all.each do |event|
+      event.dates.each do |duedate|
+        if (duedate == tomorrow) && (event.contact_method == 'both')
+          
+          # text method
+          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+          auth_token = "65796c1331a3c329820dd1f22033946e"
+          number_to_send_to = event.cell_phone
+          twilio_phone_number = "(954)-933-5130"
+          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+          @twilio_client.account.sms.messages.create(
+          :from => twilio_phone_number,
+          :to => number_to_send_to,
+          :body => "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.")
+          
+          # call method
+          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+          auth_token = "65796c1331a3c329820dd1f22033946e"
+          message = "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.Goodbye!"
+          url = "http://twimlets.com/voicemail?Message=#{URI::encode message}"
+          number_to_send_to = event.cell_phone
+          twilio_phone_number = "(954)-933-5130"
+          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+          @twilio_client.account.calls.create(
+          :from => twilio_phone_number,
+          :to => number_to_send_to,
+          :url => url)
+
+        elsif (duedate == tomorrow) && (event.contact_method == 'text')
+          # text method
+          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+          auth_token = "65796c1331a3c329820dd1f22033946e"
+          number_to_send_to = event.cell_phone
+          twilio_phone_number = "(954)-933-5130"
+          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+          @twilio_client.account.sms.messages.create(
+          :from => twilio_phone_number,
+          :to => number_to_send_to,
+          :body => "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.")
+          
+        elsif (duedate == tomorrow) && (event.contact_method == 'phone_call')
+           # call method
+          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+          auth_token = "65796c1331a3c329820dd1f22033946e"
+          message = "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.Goodbye!"
+          url = "http://twimlets.com/voicemail?Message=#{URI::encode message}"
+          number_to_send_to = event.cell_phone
+          twilio_phone_number = "(954)-933-5130"
+          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+          @twilio_client.account.calls.create(
+          :from => twilio_phone_number,
+          :to => number_to_send_to,
+          :url => url)
+        end
       end
     end
   end
-  end
-
-
-  def text
-    account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
-    auth_token = "65796c1331a3c329820dd1f22033946e"
-
-    number_to_send_to = event.cell_phone
-    twilio_phone_number = "(954)-933-5130"
-
-    @twilio_client = Twilio::REST::Client.new account_sid, auth_token
-
-    @twilio_client.account.sms.messages.create(
-    :from => twilio_phone_number,
-    :to => number_to_send_to,
-    :body => "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.")
-  end
-
-  def call
-    message = "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.Goodbye!"
-    url = "http://twimlets.com/voicemail?Message=#{URI::encode message}"
-
-    account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
-    auth_token = "65796c1331a3c329820dd1f22033946e"
-
-    number_to_send_to = event.cell_phone
-    twilio_phone_number = "(954)-933-5130"
-
-    @twilio_client = Twilio::REST::Client.new account_sid, auth_token
-
-    @twilio_client.account.calls.create(
-    :from => twilio_phone_number,
-    :to => number_to_send_to,
-    :url => url)
-  end
-
+  
 end
+
+    
+  
--- lib/tasks/remind.rake
+++ lib/tasks/remind.rake
@@ -1,55 +1,57 @@
+desc "send off text and call reminders"
+  
+  task :remind => :environment do
 
-namespace :remind do
-  desc "Looks through database and finds events due tomorrow. sends user text reminder of event"
+    puts "Second Worker Now Running"
+    SecondWorker.new.perform
+    puts "Succeed"
 
-  task text: :environment do
-    tomorrow = Date.tomorrow
-    EventRecurrence.all.each do |event|
-      event.dates.each do |duedate|
-        if (duedate == tomorrow) && (event.contact_method == 'text' || event.contact_method == 'both')
-          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
-          auth_token = "65796c1331a3c329820dd1f22033946e"
+    # tomorrow = Date.tomorrow
+    # EventRecurrence.all.each do |event|
+    #   event.dates.each do |duedate|
+    #     if (duedate == tomorrow) && (event.contact_method == 'both')
+    #       send_text
+    #       cend_call
+    #     elsif (duedate == tomorrow) && (event.contact_method == 'text')
+    #       send_text
+    #     elsif (duedate == tomorrow) && (event.contact_method == 'phone call')
+    #       send_call
+    #     end
+    #   end
+    # end
 
-          number_to_send_to = event.cell_phone
-          twilio_phone_number = "(954)-933-5130"
 
-          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+    # def send_text
+    #   account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+    #   auth_token = "65796c1331a3c329820dd1f22033946e"
 
-          @twilio_client.account.sms.messages.create(
-          :from => twilio_phone_number,
-          :to => number_to_send_to,
-          :body => "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.")
-        end 
-      end
-    end
+    #   number_to_send_to = event.cell_phone
+    #   twilio_phone_number = "(954)-933-5130"
+
+    #   @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+
+    #   @twilio_client.account.sms.messages.create(
+    #   :from => twilio_phone_number,
+    #   :to => number_to_send_to,
+    #   :body => "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.")
+    # end
+
+    # def send_call
+    #   message = "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.Goodbye!"
+    #   url = "http://twimlets.com/voicemail?Message=#{URI::encode message}"
+
+    #   account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
+    #   auth_token = "65796c1331a3c329820dd1f22033946e"
+
+    #   number_to_send_to = event.cell_phone
+    #   twilio_phone_number = "(954)-933-5130"
+
+    #   @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+
+    #   @twilio_client.account.calls.create(
+    #   :from => twilio_phone_number,
+    #   :to => number_to_send_to,
+    #   :url => url)
+    # end
+
   end
-
- desc "Looks through database and finds events due tomorrow. sends user phone call reminder of event"
- task call: :environment do
-    tomorrow = Date.tomorrow
-    EventRecurrence.all.each do |event|
-      event.dates.each do |duedate|
-        if (duedate == tomorrow) && (event.contact_method == 'phone call' || event.contact_method == 'both')
-          
-          message = "Hello #{event.first_name}! This is a friendly reminder that your #{event.provider_name}, #{event.category_name} bill is due tomorrow.Goodbye!"
-          url = "http://twimlets.com/voicemail?Message=#{URI::encode message}"
-
-          account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
-          auth_token = "65796c1331a3c329820dd1f22033946e"
-
-          number_to_send_to = event.cell_phone
-          twilio_phone_number = "(954)-933-5130"
-
-          @twilio_client = Twilio::REST::Client.new account_sid, auth_token
-
-          @twilio_client.account.calls.create(
-          :from => twilio_phone_number,
-          :to => number_to_send_to,
-          :url => url)
-        end 
-      end
-    end
-  end
-
-end
-
