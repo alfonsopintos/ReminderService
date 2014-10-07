05faeca45da6ecd0ded983bcdcd38a2dd85b32cb
alfonsopintos <alfonsopintos@gmail.com>
2014-10-07 13:58:38 -0400

    text worker running

--- app/workers/text_worker.rb
+++ app/workers/text_worker.rb
@@ -5,18 +5,18 @@ class TextWorker
 
   def perform
     tomorrow = Date.tomorrow
-    EventRecurrence.all.each do |event|     
-      event.dates.each do |duedate|
-        if (duedate == tomorrow) && (event.contact_method == 'text')
+    Text.all.each do |text|     
+      text.event_recurrence.dates.each do |duedate|
+        if (duedate == tomorrow)
           account_sid = "AC458c66afe8c3be7f362e34e212c63b84"
           auth_token = "65796c1331a3c329820dd1f22033946e"
-          number_to_send_to = event.cell_phone
+          number_to_send_to = text.cell_phone
           twilio_phone_number = "(954)-933-5130"
           @twilio_client = Twilio::REST::Client.new account_sid, auth_token
           @twilio_client.account.sms.messages.create(
           :from => twilio_phone_number,
           :to => number_to_send_to,
-          :body => "#{event.text_reminder}")
+          :body => "#{text.text_reminder}")
         end
       end
     end
--- lib/tasks/remind.rake
+++ lib/tasks/remind.rake
@@ -2,16 +2,16 @@ desc "send off text and call reminders"
   
   task :remind => :environment do
 
-    puts "Call Worker Now Running"
-    CallWorker.new.perform
-    puts "Succeed"
+    # puts "Call Worker Now Running"
+    # CallWorker.new.perform
+    # puts "Succeed"
 
     puts "Text Worker Now Running"
     TextWorker.new.perform
     puts "Succeed"
 
-    puts "Email Worker Now Running"
-    EmailWorker.new.perform
-    puts "Succeed"
+    # puts "Email Worker Now Running"
+    # EmailWorker.new.perform
+    # puts "Succeed"
 
   end
