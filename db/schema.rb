ac3db6a777135a32acca687ab15c83de89343cd5
alfonsopintos <alfonsopintos@gmail.com>
2014-10-07 13:05:55 -0400

    update and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007142649) do
+ActiveRecord::Schema.define(version: 20141007051321) do
 
   create_table "calls", force: true do |t|
     t.string   "cell_phone"
