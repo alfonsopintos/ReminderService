b10064dda2e0143d8e494b2b124a78fc64ca20e1
alfonsopintos <alfonsopintos@gmail.com>
2014-10-07 16:15:03 -0400

    call validations. Presence on both columns

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,6 +1,8 @@
 class Call < ActiveRecord::Base
   belongs_to :event_recurrence
 
+  validates :cell_phone, presence: true
   validates :cell_phone, length: { is: 10 }
   validates :cell_phone, :numericality => {:only_integer => true}
+  validates :call_reminder, presence: true
 end
