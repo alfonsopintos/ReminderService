ace32c1a2f56e5229b7374e85534375b58c0afae
Joel Lusky <joelzlusky@gmail.com>
2014-10-07 16:08:40 -0400

    api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  before_filter :restrict_access
+
+
+  private
+  
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+  end
+    end
 end
