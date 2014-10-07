1b3380febc9e2033c788e312b5585a363606550a
alfonsopintos <alfonsopintos@gmail.com>
2014-10-07 16:13:01 -0400

    email validations. Address format and Presence

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,9 @@
 class Email < ActiveRecord::Base
   belongs_to :event_recurrence
 
-  #validate email_address
+  validates :email_reminder, presence: true
+  validates :email_address, presence: true
+  validates_format_of :email_address,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
   
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,8 +1,10 @@
 class Text < ActiveRecord::Base
   belongs_to :event_recurrence
 
+  validates :cell_phone, presence: true
   validates :cell_phone, length: { is: 10 }
   validates :cell_phone, :numericality => {:only_integer => true}
+  validates :text_reminder, presence: true
   validates :text_reminder, length: => {:maximum => 160}
 
 end
