b0df0087ccb100d6808f8a122513ff988c0a5311
alfonsopintos <alfonsopintos@gmail.com>
2014-10-07 21:07:52 -0400

    refactored mailer

--- app/mailers/remind_mailer.rb
+++ app/mailers/remind_mailer.rb
@@ -2,12 +2,12 @@ class RemindMailer < ActionMailer::Base
   default from: 'fmnapp@gmail.com'
   include Sidekiq::Mailer
 
-  def email_reminder(email_instance_id)
-    @instance = Email.find(email_instance_id)
-
-    email = @instance.email
-
-    mail to: email, subject: "Bill Reminder"  
+  #send_email takes the id of the email instance it has to send as an arguement
+  def send_email(email_id)
+    #variable 'send' represents the instance of the email to send. Obtains it through the (email_id) parameter
+    send = Email.find(email_id) 
+    #tells Mailer to send email to the email_address of that instance. being represented here by send
+    mail to: send.email_address, subject: "Bill Reminder"  
   end
 
 end
