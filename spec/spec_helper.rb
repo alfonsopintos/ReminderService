483fc63e03675cee566ffd7fd7caa7f6b6595712
Joel Lusky <joelzlusky@gmail.com>
2014-10-07 20:27:24 -0400

    rspec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
+config.include Requests::JsonHelpers, :type => :controller
+config.include AuthHelpers, :type => :controller
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
