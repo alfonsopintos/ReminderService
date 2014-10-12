114218b5eb91b9df0f9306211e3df9df00877372
alfonsopintos <alfonsopintos@gmail.com>
2014-10-12 14:05:32 -0400

    root to event recurrences index

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :event_recurrences
 
+  root 'event_recurrences#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
